e2da2ffb1accdcd05f4d536d23d82dd41e35e025
Tommy Coleman <tommiilee@gmail.com>
2019-09-04 16:25:57 -0400

    installed dependancies

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,9 +1,15 @@
-const express = 'express';
-
+const express = require('express');
+const db = require('./postDb.js')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    db.find()
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
 });
 
 router.get('/:id', (req, res) => {
