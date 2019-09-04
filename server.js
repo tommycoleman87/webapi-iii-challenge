4344edef5f0444acfc79216e54f42aa9515b624b
Tommy Coleman <tommiilee@gmail.com>
2019-09-04 18:49:05 -0400

    connected user middleware and endpoints

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const userRoutes = require('./users/userRouter.js');
 const postsRoutes = require('./posts/postRouter.js');
 
 server.use(express.json())
-server.use('/users', userRoutes);
-server.use('/posts', postsRoutes);
+
+server.use('/posts',logger, postsRoutes);
+server.use('/users',logger, userRoutes);
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
@@ -14,7 +15,8 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(req.method, req.originalUrl, new Date())
+  next();
 };
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,115 @@
 const express = require('express');
-
+const db = require('./userDb.js');
+const Posts = require('../posts/postDb.js');
 const router = express.Router();
-
-router.post('/', (req, res) => {
-
+router.use(express.json());
+router.post('/', validateUser, (req, res) => {
+    const user = req.body;
+   db.insert(user)
+   .then(result => {
+       res.status(201).json(result)
+   })
+   .catch(err => {
+       res.status(500).json({error: 'Could not create user'})
+   })
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.post('/:id/posts', validatePost, validateUserId, (req, res) => {
+    const post = req.body;
+    Posts.insert(post)
+    .then(result => {
+        res.status(201).json(result)
+    })
+    .catch(err => {
+        res.status(500).json({error: err})
+    })
 });
 
 router.get('/', (req, res) => {
-
+    db.get()
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId, (req, res) => {
+    db.getById(req.params.id)
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        res.status(500).json({error: "Could not retrieve user"})
+    })
 });
 
-router.get('/:id/posts', (req, res) => {
-
+router.get('/:id/posts', validateUserId, (req, res) => {
+    db.getUserPosts(req.params.id)
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        res.status(500).json({error: 'Could not retrieve posts'})
+    })
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validateUserId, (req, res) => {
+    db.remove(req.params.id)
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        res.status(500).json({error: 'Could not delete user'})
+    })
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validateUserId, validateUser, (req, res) => {
+    const user = req.body;
+    db.update(req.params.id, user)
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        res.status(500).json({error: 'Could not update user'})
+    })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-
+    db.getById(req.params.id)
+    .then(result => {
+        if(result) {
+            req.user = result;
+            console.log(req.user)
+            next();
+        } else {
+            res.status(404).json({ message: "invalid user id" })
+        }
+    })
 };
 
 function validateUser(req, res, next) {
-
+    const user = req.body;
+    if(!user) {
+        res.status(400).json({message: 'Missing user data'})
+    } else if(!user.name){
+        res.status(400).json({message: 'Missing required name field'})
+    } else {
+        next();
+    }
 };
 
 function validatePost(req, res, next) {
-
+    const post = req.body
+    post.user_id = req.params.id
+    if(!post.text) {
+        res.status(400).json({ message:"missing required text field"  })
+    } else {
+        next();
+    }
 };
 
 module.exports = router;
