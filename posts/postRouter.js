5dfd13d9b9d7cd7e466361b3f53ccea9cfd51f96
Tommy Coleman <tommiilee@gmail.com>
2019-09-04 16:51:58 -0400

    implemented middleware and routing for posts

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -3,7 +3,7 @@ const db = require('./postDb.js')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    db.find()
+    db.get()
     .then(result => {
         res.status(200).json(result)
     })
@@ -12,22 +12,56 @@ router.get('/', (req, res) => {
     })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validatePostId, (req, res) => {
+    const id = req.params.id;
+    db.getById(id)
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        res.status(500).json({error: ''})
+    })
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validatePostId, (req, res) => {
+    db.remove(req.params.id)
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        res.status(500).json({error: 'Can not delete Post'})
+    })
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validatePostId, (req, res) => {
+    const post = req.body
+    if(post.text) {
+    db.update(req.params.id, post)
+    .then(result => {
+        res.status(200).json(post)
+    })
+    .catch(err => {
+        res.status(500).json({error: 'Can not update post'})
+    })
+    } else {
+        res.status(400).json({error: 'Please include text in post'})
+    }
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-
+    db.getById(req.params.id)
+    .then(result => {
+        if(result){
+            next()
+        } else {
+            res.status(404).json({ message: "invalid user id" })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({error: 'Could not validate ID'})
+    })
 };
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 
 const server = express();
+const userRoutes = require('./users/userRouter.js');
+const postsRoutes = require('./posts/postRouter.js');
+
 server.use(express.json())
+server.use('/users', userRoutes);
+server.use('/posts', postsRoutes);
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
