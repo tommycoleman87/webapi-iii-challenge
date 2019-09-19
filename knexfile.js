9850e742641225edecc1865a6098f99e755fb3c3
Tommy Coleman <tommiilee@gmail.com>
2019-09-19 13:58:14 -0400

    add pg

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,7 @@
 module.exports = {
   development: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './data/blog.db3',
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations',
       tableName: 'knex_migrations',
@@ -19,3 +11,6 @@ module.exports = {
     },
   },
 };
+
+
+//postgres://qeyrktutzuxlow:cf9dfbd91198ea64a8eca488f180648f1bca2b73233464c7b5cc86233a98b9fe@ec2-107-20-167-241.compute-1.amazonaws.com:5432/desgo5oouk56es
